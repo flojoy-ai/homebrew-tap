91c89d3ba1cee281a3869e04f5d1da950c32099b
Joey Yu <joey@jyu.dev>
2023-12-09 14:47:30 -0500

    feat: add studiolab

---
+++ studiolab.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+cask 'studiolab' do
+  version '0.0.1' # Specify the version you want to download
+  # sha256 "..."   # Add the SHA256 hash of the release, if available
+
+  url "https://github.com/flojoy-ai/studiolab/releases/download/v#{version}/flojoy-studio-#{version}.dmg"
+  # Replace with the URL to the release asset
+
+  name 'Flojoy Studio' # Replace with the name of your app
+  desc 'The next-generation Flojoy Studio.' # Provide a brief description
+
+  app 'flojoy-studio.app' # Replace with the actual application file or folder
+
+  # Specify any additional dependencies or conflicts if needed
+  depends_on macos: '>= :high_sierra'
+end
